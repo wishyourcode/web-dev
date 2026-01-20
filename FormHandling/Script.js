be04cc1a8c035b831afb41ee86f0e7c85a98a6c5
wishyourcode <vishalm3101@gmail.com>
2026-01-20 22:58:16 +0530

    ui decorated

--- FormHandling/Script.js
+++ FormHandling/Script.js
@@ -1,5 +1,11 @@
-let name = document.querySelector("#name");
+let nam = document.querySelector("#name");
 let email = document.querySelector("#email");
 let pass = document.querySelector("#password");
 let area = document.querySelector("#textarea");
 let form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (nam.value.length < 5) {
+    console.log("name length should be greater than 5");
+  }
+});
