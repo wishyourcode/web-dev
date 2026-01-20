df5f8ca38c936f33a450cb70c605e5651c4f4faf
wishyourcode <vishalm3101@gmail.com>
2026-01-20 21:58:14 +0530

    form ui created

---
+++ FormHandling/Script.js
@@ -0,0 +1,5 @@
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let pass = document.querySelector("#password");
+let area = document.querySelector("#textarea");
+let form = document.querySelector("form");
