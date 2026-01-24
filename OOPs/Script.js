e256c84b4fc6b6ea72ed581786f0e0988422b6ed
wishyourcode <vishalm3101@gmail.com>
2026-01-24 16:16:28 +0530

    class and constructor

---
+++ OOPs/Script.js
@@ -0,0 +1,21 @@
+class CreatePencil {
+  constructor(name, company, price, color) {
+    this.name = name;
+    this.company = company;
+    this.price = price;
+    this.color = color;
+  }
+  write(text) {
+    let h2 = document.createElement("h2");
+    h2.textContent = text;
+    h2.style.color = this.color;
+    document.body.appendChild(h2);
+  }
+}
+
+let p1 = new CreatePencil("Apsara", "Apsara ", 20, "red");
+let p2 = new CreatePencil("Natraj", "Natraj ", 20, "yellow");
+let p3 = new CreatePencil("Apsara", "Apsara ", 20, "pink");
+p1.write("Radha Rani ji");
+p2.write("Govind");
+p3.write("Madhav");
